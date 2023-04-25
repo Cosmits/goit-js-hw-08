2ba5b7a7c8c619a24b5c1f666799f37f86eb40ce
Cosmits <salge.cos@gmail.com>
2023-04-25 12:41:54 +0200

    task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,36 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryUlEl = document.querySelector('.gallery');
+galleryUlEl.insertAdjacentHTML('beforeend', doGalleryList(galleryItems));
+
+// Initialize SimpleLightbox
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    animationSpeed: 250,
+});
+
+//
+//~ any functions 
+//
+function doGalleryList(arrGalleryItems) {
+    return arrGalleryItems.map(({ preview, original, description }) => {
+        //const { preview, origin, description } = el;
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>
+    </li>`
+    }).join('');
+}
+
